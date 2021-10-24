d0b28dbea799211910ea956bec9e57b0b2c50d25
sbalachandar1 <sbalachandar@deloitte.com>
2021-10-24 14:47:14 -0400

    Playing around with the slider

--- sudoku/src/components/SliderInput.tsx
+++ sudoku/src/components/SliderInput.tsx
@@ -4,8 +4,17 @@ import Slider, { createSliderWithTooltip } from 'rc-slider';
 
 const CreateSliderWithTooltip = createSliderWithTooltip(Slider);
 
+const marks = {
+  1: '1ms',
+  100: '100ms',
+  200: '200ms',
+  300: '300ms',
+  400: '400ms',
+  500: '500ms'
+};
+
 const msFormatter = (v: any) => {
-  return `${v}ms`
+  return `${501 - v}ms`
 }
 
 const consoleLog = (v: any) => {
@@ -20,7 +29,19 @@ const SliderInput: React.FC<any> = () => {
       <CreateSliderWithTooltip
         min={1}
         max={500}
+        defaultValue={251}
+        railStyle={{ backgroundColor: 'rgb(252, 206, 147)', height: 5 }}
+        handleStyle={{
+          borderColor: 'red',
+          height: 14,
+          width: 14,
+          marginLeft: 0,
+          marginTop: -5,
+          backgroundColor: 'black',
+        }}
         className='slider'
+        included={false}
+        marks={marks}
         tipFormatter={msFormatter}
         onChange={consoleLog}
        />
